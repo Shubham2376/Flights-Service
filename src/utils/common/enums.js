aaa4fbe17bb3a5b0303ebdec65bc8a86a4d6b43b
MansiGupta888 <shubhamtalukdar06@gmail.com>
2025-09-16 00:48:41 +0530

    Added Seats model in our db associated to airplane

---
+++ src/utils/common/enums.js
@@ -0,0 +1,10 @@
+const SEAT_TYPE = {
+    BUSINESS : 'business',
+    ECONOMY : 'economy',
+    PREMIUM_ECONOMY : 'premium-economy',
+    FIRST_CLASS : 'first-class'
+}
+// There can be more enums which comes so that is why i return as object 
+module.exports = {
+    SEAT_TYPE
+}
